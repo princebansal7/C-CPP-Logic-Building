eb7b4bbdce7cc3709b997684410d11b13fd55eb3
Prince Bansal <prince.bansal008@gmail.com>
2022-09-27 11:55:14 +0530

    modified

--- C-language/Assignment-1/5.LengthOfStingUsingPrintf.c
+++ C-language/Assignment-1/5.LengthOfStingUsingPrintf.c
@@ -3,8 +3,8 @@
 int main(){
     printf("\n\n"); 
 
-    int length = printf("Prince Bansal\n");
-    printf("length of \"Prince Bansal\" is %d",length);
+    int length = printf("Prince Bansal");
+    printf("\nlength of \"Prince Bansal\" is %d",length);
 
     printf("\n\n");
     return 0;
