c5709886c3b3d8fc5dbc2ffcdec0bb3fafd9e78c
Prince Bansal <prince.bansal008@gmail.com>
2022-10-05 02:21:35 +0530

    Assignment-7, ques-3 remains

---
+++ C-language/Assignment-7 (Loops Mixed-2)/3[doubt].c
@@ -0,0 +1,12 @@
+// A number is Fibonacci if and only if one or both of (5*n2 + 4) or (5*n2 – 4) is a perfect square
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    return 0;
+}
---
+++ C-language/Assignment-7 (Loops Mixed-2)/7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// all prime numbers between a and b
+int main()
+{
+    int a, b;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("All prime numbers between %d and %d are:\n", a, b);
+    for (int i = a; i <= b; i++) {
+        int flag = 1;
+        if (i == 1)
+            flag = 0;
+        else {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+        if (flag == 1) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
