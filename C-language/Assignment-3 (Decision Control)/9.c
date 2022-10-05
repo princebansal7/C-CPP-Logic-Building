7de57eb6628a27abc44a79525aac0ced41c877c2
Prince Bansal <prince.bansal008@gmail.com>
2022-10-05 13:04:11 +0530

    Optimsed logic added

--- C-language/Assignment-3 (Decision Control)/9.c
+++ C-language/Assignment-3 (Decision Control)/9.c
@@ -18,23 +18,22 @@ int main()
 
     /* More Effiecient
 
-        if (a > b) {
+        if (a > b)
             if (a > c)
                 printf("%d\n", a);
             else
                 printf("%d\n", c);
-        } else {
+        else
             if (b > c)
                 printf("%d\n", b);
             else
                 printf("%d\n", c);
-        }
 
     */
 
     // Using Ternary
 
-    int ans = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    int ans = a > b ? (a > c ? a : c) : (b > c ? b : c);
     printf("%d\n", ans);
 
     return 0;
