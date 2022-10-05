b09fc4bce76bad679fbff2849d9933583f9bf2a8
Prince Bansal <prince.bansal008@gmail.com>
2022-10-05 12:19:55 +0530

    Optimsed logic added

--- C-language/Assignment-3 (Decision Control)/9.c
+++ C-language/Assignment-3 (Decision Control)/9.c
@@ -6,11 +6,36 @@ int main()
     printf("Enter 3 number a, b & c: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a >= b && a >= c)
-        printf("a is greater\n");
-    else if (b >= c)
-        printf("b is greater\n");
-    else
-        printf("c is greater\n");
+    /* Less efficient logic
+
+           if (a > b && a > c)
+               printf("%d\n", a);
+           else if (b > c)
+               printf("%d\n", b);
+           else
+               printf("%d\n", c);
+   */
+
+    /* More Effiecient
+
+        if (a > b) {
+            if (a > c)
+                printf("%d\n", a);
+            else
+                printf("%d\n", c);
+        } else {
+            if (b > c)
+                printf("%d\n", b);
+            else
+                printf("%d\n", c);
+        }
+
+    */
+
+    // Using Ternary
+
+    int ans = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+    printf("%d\n", ans);
+
     return 0;
 }
