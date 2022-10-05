0c7c4b47ba909bf152dc2cef001774ce6e83e6b7
Prince Bansal <prince.bansal008@gmail.com>
2022-10-05 13:26:02 +0530

    doubt solved

---
+++ C-language/Assignment-7 (Loops Mixed-2)/3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, flag = 0, fTerm = 0, sTerm = 1;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    int sum = 0;
+    if (n == 0)
+        flag = 1;
+    else {
+        for (int i = 1; i <= n; i++) {
+            sum = fTerm + sTerm;
+            if (n == sum) {
+                flag = 1;
+                break;
+            }
+            fTerm = sTerm;
+            sTerm = sum;
+        }
+    }
+    if (flag == 1)
+        printf("\n %d exits in Fibinacci series\n", n);
+    else
+        printf("\n%d doesn't exits\n", n);
+    printf("\n");
+    return 0;
+}
