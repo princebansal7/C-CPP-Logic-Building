c5fdd7d3f8f924410df084be7d99678e627f997d
Prince Bansal <prince.bansal008@gmail.com>
2022-11-02 20:57:19 +0530

    code updated

--- C-language/Assignment-9 (Switch-Case)/6.c
+++ C-language/Assignment-9 (Switch-Case)/6.c
@@ -2,16 +2,32 @@
 #include <stdio.h>
 int main()
 {
-    // Menu Driven Program
     int year;
-
     printf("Enter year: ");
     scanf("%d", &year);
 
-    switch (year) {
-    // Yet to do
-    case 1900 ... 3000:
-        printf("%d\n", year);
+    switch (year % 100 == 0) { // condition true => 1, else 0
+    case 1:
+        switch (year % 400 == 0) {
+        case 1:
+            printf("%d is Leap year\n", year);
+            break;
+        case 0:
+            printf("%d is not Leap year\n", year);
+            break;
+        }
+        break;
+    case 0:
+        switch (year % 4 == 0) {
+        case 1:
+            printf("%d is Leap year\n", year);
+            break;
+
+        case 0:
+            printf("%d is not Leap year\n", year);
+            break;
+        }
+        break;
     }
     return 0;
 }
