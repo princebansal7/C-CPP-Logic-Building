68c5b01b5e75ba05ba07a637cedecec9ab7f72db
Prince Bansal <prince.bansal008@gmail.com>
2022-11-29 02:10:08 +0530

    pointer size logic updated

--- test.c
+++ test.c
@@ -5,14 +5,28 @@ int main()
 
     int a[] = { 1, 2, 3, 4, 5 };
     int* ptr = a;
-    char* ptr1 = a;
+    char* ptr1 = a; // compiler gives warning
 
     // why pointer variable should of same type as the type who's
     // address it's keeping
 
     printf("\n%d %d\n", *ptr, *ptr1); // O/P: 1 1
+
     ptr++; // will point to 2 (as it knows to move by 4 bytes)
     ptr1++; // will move 1 byte only so unpredictable value
+
     printf("%d %d\n", *ptr, *ptr1); // O/P: 2 (unpredictable value)
+
+    // Checking size
+    int* p1;
+    double* p2;
+    char* p3;
+    float* p4;
+
+    printf("size of \"int pointer\": %ld\n", sizeof(p1)); // 8
+    printf("size of \"double pointer\": %ld\n", sizeof(p2)); // 8
+    printf("size of \"char pointer\": %ld\n", sizeof(p3)); // 8
+    printf("size of \"float pointer\": %ld\n", sizeof(p4)); // 8
+
     return 0;
 }
