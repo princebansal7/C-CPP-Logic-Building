d66fcf7a1f51fd733cfb5f15200c07585a239d3e
Prince Bansal <prince.bansal008@gmail.com>
2022-12-21 02:06:09 +0530

    C string testing

--- test.c
+++ test.c
@@ -1,43 +1,53 @@
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
+    char str[20] = "prince";
 
-    if (__STDC_VERSION__ >= 201710L)
-        printf("We are using C18!\n");
-    else if (__STDC_VERSION__ >= 201112L)
-        printf("We are using C11!\n");
-    else if (__STDC_VERSION__ >= 199901L)
-        printf("We are using C99!\n");
-    else
-        printf("We are using C89/C90!\n");
+    printf("Enter String: ");
+    fgets(str, 20, stdin);
+
+    printf("%s", strupr(str));
 
     /*
 
-       int a[] = { 1, 2, 3, 4, 5 };
-       int* ptr = a;
-       char* ptr1 = a; // compiler gives warning
+    //-----------------------------------------------------
 
-       // why pointer variable should of same type as the type who's
-       // address it's keeping
+        if (__STDC_VERSION__ >= 201710L)
+            printf("We are using C18!\n");
+        else if (__STDC_VERSION__ >= 201112L)
+            printf("We are using C11!\n");
+        else if (__STDC_VERSION__ >= 199901L)
+            printf("We are using C99!\n");
+        else
+            printf("We are using C89/C90!\n");
 
-       printf("\n%d %d\n", *ptr, *ptr1); // O/P: 1 1
+    // ----------------------------------------------------
 
-       ptr++; // will point to 2 (as it knows to move by 4 bytes)
-       ptr1++; // will move 1 byte only so unpredictable value
+           int a[] = { 1, 2, 3, 4, 5 };
+           int* ptr = a;
+           char* ptr1 = a; // compiler gives warning
 
-       printf("%d %d\n", *ptr, *ptr1); // O/P: 2 (unpredictable value)
+           // why pointer variable should of same type as the type who's
+           // address it's keeping
 
-       // Checking size
-       int* p1;
-       double* p2;
-       char* p3;
-       float* p4;
+           printf("\n%d %d\n", *ptr, *ptr1); // O/P: 1 1
 
-       printf("size of \"int pointer\": %ld\n", sizeof(p1)); // 8
-       printf("size of \"double pointer\": %ld\n", sizeof(p2)); // 8
-       printf("size of \"char pointer\": %ld\n", sizeof(p3)); // 8
-       printf("size of \"float pointer\": %ld\n", sizeof(p4)); // 8
-   */
+           ptr++; // will point to 2 (as it knows to move by 4 bytes)
+           ptr1++; // will move 1 byte only so unpredictable value
+
+           printf("%d %d\n", *ptr, *ptr1); // O/P: 2 (unpredictable value)
+
+           // Checking size
+           int* p1;
+           double* p2;
+           char* p3;
+           float* p4;
+
+           printf("size of \"int pointer\": %ld\n", sizeof(p1)); // 8
+           printf("size of \"double pointer\": %ld\n", sizeof(p2)); // 8
+           printf("size of \"char pointer\": %ld\n", sizeof(p3)); // 8
+           printf("size of \"float pointer\": %ld\n", sizeof(p4)); // 8
+       */
     return 0;
 }
