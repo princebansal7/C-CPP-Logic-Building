b2140e34bd23cf962316fe1e6998c93e78388d35
Prince Bansal <prince.bansal008@gmail.com>
2023-05-24 18:40:13 +0530

    code updated

--- C++ Language/Assignment-26 (Member, Static, Constructor)/2.ComplexProper.cpp
+++ C++ Language/Assignment-26 (Member, Static, Constructor)/2.ComplexProper.cpp
@@ -30,15 +30,22 @@ public:
     }
     void showData()
     {
-        cout << "Real number: " << real << nl;
-        cout << "Imaginary number: " << imag << nl;
+        // cout << "Real number: " << real << nl;
+        // cout << "Imaginary number: " << imag << nl;
         if (imag >= 0)
             cout << "Complex number: " << real << " + " << imag << "i" << nl;
         else
-            cout << "Complex number: " << real << " " << imag << "i" << nl;
+            cout << "Complex number: " << real << " - " << (0 - imag) << "i" << nl;
     }
 
     Complex add(Complex& temp)
+    {
+        Complex x;
+        x.real = temp.real + real;
+        x.imag = temp.imag + imag;
+        return x;
+    }
+    Complex operator+(Complex& temp)
     {
         Complex x;
         x.real = temp.real + real;
@@ -50,13 +57,14 @@ public:
 int main()
 {
     Complex c1, c3;
-    c1.setData(6, 9);
+    c1.setData(-6, -9);
     c1.showData();
 
-    Complex c2(2, -6);
+    Complex c2(2, 6);
     c2.showData();
 
-    c3 = c1.add(c2);
+    // c3 = c1.add(c2);
+    c3 = c1 + c2;
     c3.showData();
 
     return 0;
