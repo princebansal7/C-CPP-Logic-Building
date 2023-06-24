ea56e7ce173c73335f9c6436c9338f8d02686982
Prince Bansal <prince.bansal008@gmail.com>
2023-06-24 23:26:34 +0530

    "file added"

---
+++ C++ Language/Assignment-26 (Member, Static, Constructor)/3.CubeConstructor.cpp
@@ -0,0 +1,32 @@
+/*
+    @author: princebansal_
+*/
+
+#include <iostream>
+using namespace std;
+#define nl '\n'
+
+class Cube {
+private:
+    int side;
+
+public:
+    Cube(int x)
+    {
+        side = x;
+    }
+
+    int getVolume()
+    {
+        return side * side * side;
+    }
+};
+
+int main()
+{
+    Cube c1(3), c2(5);
+    cout << "Volume is Cube 1 is: " << c1.getVolume() << nl;
+    cout << "Volume is Cube 2 is: " << c2.getVolume() << nl;
+
+    return 0;
+}
