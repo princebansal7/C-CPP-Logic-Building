58b38819034968b87bd6d32e760d16bc631bf8e2
Prince Bansal <prince.bansal008@gmail.com>
2023-10-07 14:25:43 +0530

    function returning multiple values - using pointers added in C & C++ both

--- C++ Language/Assignment-23 (Basics of C++)/11.FunctionReturningMultipleValues.cpp
+++ C++ Language/Assignment-23 (Basics of C++)/11.FunctionReturningMultipleValues.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define nl '\n'
 
-void getSum(int a, int b, int* sum, int* prod)
+void getSumAndProd(int a, int b, int* sum, int* prod)
 {
     // Not literally returning multiple values
     // But just manipulating them using pointer outside of their scope (in another function)
@@ -16,7 +16,7 @@ int main()
     cout << "Enter a and b value: ";
     cin >> a >> b;
 
-    getSum(a, b, &SUM, &PROD);
+    getSumAndProd(a, b, &SUM, &PROD);
 
     cout << "Sum of a & b is: " << SUM << nl;
     cout << "Product of a & b is: " << PROD << nl;
