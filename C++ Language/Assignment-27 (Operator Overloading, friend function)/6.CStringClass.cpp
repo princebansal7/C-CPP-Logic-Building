f9ae05f42519ffea7fc29e0c2bd36946e8df8e19
Prince Bansal <prince.bansal008@gmail.com>
2023-10-10 00:05:17 +0530

    c style string concatination done using operator+ overloading

---
+++ C++ Language/Assignment-27 (Operator Overloading, friend function)/6.CStringClass.cpp
@@ -0,0 +1,53 @@
+/*
+    @author: princebansal_
+*/
+
+#include <cstring>
+#include <iostream>
+using namespace std;
+#define nl '\n'
+#define MAX 100
+
+class CString {
+private:
+    char str[MAX];
+
+public:
+    void setString()
+    {
+        cout << "Enter string: ";
+        // cin.ignore();
+        cin.getline(str, 100);
+    }
+
+    void getString()
+    {
+        cout << str << nl;
+    }
+
+    // Defining custom + operator to concatinate C style strings
+    CString operator+(CString S2)
+    {
+        CString ans;
+        strcpy(ans.str, str); // copying data of s1 (caller) to temporary ans string
+        strcat(ans.str, S2.str); // concatinating passed argument (s2 string) with new string and returning the result
+        return ans;
+    }
+};
+
+int main()
+{
+    CString s1;
+    s1.setString();
+    s1.getString();
+
+    CString s2;
+    s2.setString();
+    s2.getString();
+
+    CString s3 = s1 + s2;
+    cout << "After concatination s1 and s2 string: ";
+    s3.getString();
+
+    return 0;
+}
