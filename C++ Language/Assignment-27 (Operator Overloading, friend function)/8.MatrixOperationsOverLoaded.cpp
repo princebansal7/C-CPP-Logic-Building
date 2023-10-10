786c09f1121c0e5df9be684db54beddd8af08017
Prince Bansal <prince.bansal008@gmail.com>
2023-10-10 22:17:33 +0530

    file update with binary (+, -) and unary (-) operator overloaded

--- C++ Language/Assignment-27 (Operator Overloading, friend function)/8.MatrixOperationsOverLoaded.cpp
+++ C++ Language/Assignment-27 (Operator Overloading, friend function)/8.MatrixOperationsOverLoaded.cpp
@@ -43,6 +43,26 @@ public:
 
         return ans;
     }
+
+    Matrix operator+(Matrix M)
+    {
+        Matrix ans;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                ans.matrix[i][j] = matrix[i][j] + M.matrix[i][j];
+
+        return ans;
+    }
+
+    Matrix operator-(Matrix M)
+    {
+        Matrix ans;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                ans.matrix[i][j] = matrix[i][j] - M.matrix[i][j];
+
+        return ans;
+    }
 };
 
 int main()
@@ -50,11 +70,25 @@ int main()
     Matrix m1;
     m1.showMatrix();
     m1.setMatrix();
+    cout << "m1 Matrix:" << nl;
+    m1.showMatrix();
+
     Matrix m2 = -m1;
+    cout << "m2 Matrix:" << nl;
     m2.showMatrix();
 
     Matrix m3;
     m3 = m2.operator-();
+    cout << "m3 Matrix:" << nl;
     m3.showMatrix();
+
+    Matrix m4 = m3 + m1;
+    cout << "m4 Matrix:" << nl;
+    m4.showMatrix();
+
+    Matrix m5 = m2 - m1;
+    cout << "m5 Matrix:" << nl;
+    m5.showMatrix();
+
     return 0;
 }
