d74ccea944af3eafafce52b3c4a99e9a0bc59c62
Prince Bansal <prince.bansal008@gmail.com>
2023-10-11 21:41:48 +0530

    updated

--- test.cpp
+++ test.cpp
@@ -13,17 +13,22 @@ using namespace std;
 int main()
 {
 
-    if (__cplusplus == 202002L)
-        cout << "C++20\n";
-    else if (__cplusplus == 201703L)
-        cout << "C++17\n";
-    else if (__cplusplus == 201402L)
-        cout << "C++14\n";
-    else if (__cplusplus == 201103L)
-        cout << "C++11\n";
-    else if (__cplusplus == 199711L)
-        cout << "C++98\n";
-    else
-        cout << "pre-standard C++\n";
+    // if (__cplusplus == 202002L)
+    //     cout << "C++20\n";
+    // else if (__cplusplus == 201703L)
+    //     cout << "C++17\n";
+    // else if (__cplusplus == 201402L)
+    //     cout << "C++14\n";
+    // else if (__cplusplus == 201103L)
+    //     cout << "C++11\n";
+    // else if (__cplusplus == 199711L)
+    //     cout << "C++98\n";
+    // else
+    //     cout << "pre-standard C++\n";
+
+    // int n = 1,2,3; // error
+    int x = (1, 2, 3);
+    cout << "x: " << x << nl;
+
     return 0;
 }
